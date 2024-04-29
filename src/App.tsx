bce6f0a11d9fa6860d06f08c789285aebc9da060
Rodrigo Fontes <rodrigo_fnts@hotmail.com>
2024-04-29 11:58:05 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export default function App() {
-  return <h1>Olá Mundo</h1>
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
