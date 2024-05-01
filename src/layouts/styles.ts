f03c6d3b1436fb1298d85a8967d684f70bebcb35
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-05-01 18:07:47 -0300

    refactor(styles): add possibility to work with fluid container

--- src/layouts/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const LayoutContainer = styled.div`
-    width: 100%;
-    max-width: calc(70rem + 4.5rem);
-    margin: 0 auto;
-    padding-right: 2.25rem;
-    padding-left: 2.25rem;
-`
