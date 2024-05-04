44df54c152ff237392c7b55ab163953f4530c508
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-05-03 21:13:19 -0300

    feat(home): add catalog filter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { ProductCard } from '../../components/ProductCard'
 import { Hero } from './components/Hero'
-import { Catalog, Items } from './styles'
+import { Catalog, Items, Heading } from './styles'
 
 export function Home() {
     return (
@@ -8,7 +8,17 @@ export function Home() {
             <Hero />
 
             <Catalog>
-                <h1>Nossos cafés</h1>
+                <Heading>
+                    <h1>Nossos cafés</h1>
+
+                    <ul>
+                        <li>Tradicional</li>
+                        <li>Especial</li>
+                        <li>Com leite</li>
+                        <li>Alcoólico</li>
+                        <li>Gelado</li>
+                    </ul>
+                </Heading>
 
                 <Items>
                     <ProductCard />
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,13 +5,46 @@ export const Catalog = styled.section`
     padding: 2rem 2.25rem 11.25rem;
     margin-right: auto;
     margin-left: auto;
+`
+
+export const Heading = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    column-gap: 1rem;
+    row-gap: 2rem;
+    margin-bottom: 4rem;
 
     h1 {
         color: ${(props) => props.theme['gray-800']};
         font-size: 2rem;
         font-weight: 800;
         line-height: 1.3;
-        margin-bottom: 4.125rem;
+    }
+
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+
+        li {
+            list-style: none;
+            color: ${(props) => props.theme['yellow-800']};
+            font-size: 0.625rem;
+            font-weight: 700;
+            text-transform: uppercase;
+            padding: 0.375rem 0.75rem;
+            border: 1px solid ${(props) => props.theme['yellow-500']};
+            border-radius: 1rem;
+            cursor: pointer;
+            transition: all 300ms ease-in-out;
+
+            &:hover {
+                background-color: ${(props) => props.theme['yellow-500']};
+                color: ${(props) => props.theme['white']};
+            }
+        }
     }
 `
 
