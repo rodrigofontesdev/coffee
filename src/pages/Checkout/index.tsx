d4e0dbe78495fc74f2abb890021dcc8b81c19187
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-05-05 20:11:26 -0300

    feat(page): create checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,250 @@
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money, Trash } from '@phosphor-icons/react'
+import { ButtonPrimary } from '../../components/ButtonPrimary'
+import { ButtonSecondary } from '../../components/ButtonSecondary'
+import { InputNumber } from '../../components/Forms/InputNumber'
+import {
+    Address,
+    AddressFormFields,
+    CartDetails,
+    CartSubtotal,
+    CheckControl,
+    CheckGroup,
+    CheckLabel,
+    CheckoutForm,
+    Container,
+    Delivery,
+    InputControl,
+    InputGroup,
+    InputLabel,
+    PaymentMethods,
+    ProductActions,
+    ProductCardSimple,
+    ProductInfo,
+    ProductTitle,
+    Subtotal,
+} from './styles'
+
+import productImg from '../../assets/images/products/expresso-tradicional.png'
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+    return (
+        <main>
+            <Container>
+                <CheckoutForm>
+                    <Delivery>
+                        <h2>Complete seu pedido</h2>
+
+                        <Address>
+                            <header>
+                                <MapPinLine size={22} />
+                                <div>
+                                    <h3>Endereço de Entrega</h3>
+                                    <p>Informe o endereço onde deseja receber seu pedido</p>
+                                </div>
+                            </header>
+
+                            <AddressFormFields>
+                                <InputGroup $cols={5}>
+                                    <InputLabel htmlFor="zipcode" $srOnly>
+                                        CEP
+                                    </InputLabel>
+                                    <InputControl
+                                        type="tel"
+                                        name="zipcode"
+                                        id="zipcode"
+                                        placeholder="CEP"
+                                        required
+                                    />
+                                </InputGroup>
+
+                                <InputGroup $cols={12}>
+                                    <InputLabel htmlFor="street" $srOnly>
+                                        Rua
+                                    </InputLabel>
+                                    <InputControl
+                                        type="text"
+                                        name="street"
+                                        id="street"
+                                        placeholder="Rua"
+                                        required
+                                    />
+                                </InputGroup>
+
+                                <InputGroup $cols={5}>
+                                    <InputLabel htmlFor="streetNumber" $srOnly>
+                                        Número
+                                    </InputLabel>
+                                    <InputControl
+                                        type="text"
+                                        name="streetNumber"
+                                        id="streetNumber"
+                                        placeholder="Número"
+                                        required
+                                    />
+                                </InputGroup>
+
+                                <InputGroup $cols={7}>
+                                    <InputLabel htmlFor="complement" $srOnly>
+                                        Complemento
+                                    </InputLabel>
+                                    <InputControl
+                                        type="text"
+                                        name="complement"
+                                        id="complement"
+                                        placeholder="Complemento"
+                                    />
+                                    <span>Opcional</span>
+                                </InputGroup>
+
+                                <InputGroup $cols={5}>
+                                    <InputLabel htmlFor="neighborhood" $srOnly>
+                                        Bairro
+                                    </InputLabel>
+                                    <InputControl
+                                        type="text"
+                                        name="neighborhood"
+                                        id="neighborhood"
+                                        placeholder="Bairro"
+                                        required
+                                    />
+                                </InputGroup>
+
+                                <InputGroup $cols={5}>
+                                    <InputLabel htmlFor="city" $srOnly>
+                                        Cidade
+                                    </InputLabel>
+                                    <InputControl
+                                        type="text"
+                                        name="city"
+                                        id="city"
+                                        placeholder="Cidade"
+                                        required
+                                    />
+                                </InputGroup>
+
+                                <InputGroup $cols={2}>
+                                    <InputLabel htmlFor="state" $srOnly>
+                                        UF
+                                    </InputLabel>
+                                    <InputControl
+                                        type="text"
+                                        name="state"
+                                        id="state"
+                                        placeholder="UF"
+                                        maxLength={2}
+                                        pattern="[a-z]+"
+                                        required
+                                    />
+                                </InputGroup>
+                            </AddressFormFields>
+                        </Address>
+
+                        <PaymentMethods>
+                            <header>
+                                <CurrencyDollar size={22} />
+                                <div>
+                                    <h3>Pagamento</h3>
+                                    <p>
+                                        O pagamento é feito na entrega. Escolha a forma que deseja
+                                        pagar
+                                    </p>
+                                </div>
+                            </header>
+
+                            <div>
+                                <CheckGroup>
+                                    <CheckControl
+                                        type="radio"
+                                        name="paymentMethod"
+                                        id="creditCard"
+                                    />
+                                    <CheckLabel htmlFor="creditCard">
+                                        <CreditCard size={16} />
+                                        Cartão de crédito
+                                    </CheckLabel>
+                                </CheckGroup>
+
+                                <CheckGroup>
+                                    <CheckControl
+                                        type="radio"
+                                        name="paymentMethod"
+                                        id="debitCard"
+                                    />
+                                    <CheckLabel htmlFor="debitCard">
+                                        <Bank size={16} />
+                                        Cartão de débito
+                                    </CheckLabel>
+                                </CheckGroup>
+
+                                <CheckGroup>
+                                    <CheckControl type="radio" name="paymentMethod" id="inCash" />
+                                    <CheckLabel htmlFor="inCash">
+                                        <Money size={16} />
+                                        Dinheiro
+                                    </CheckLabel>
+                                </CheckGroup>
+                            </div>
+                        </PaymentMethods>
+                    </Delivery>
+
+                    <CartDetails>
+                        <h2>Cafés selecionados</h2>
+
+                        <CartSubtotal>
+                            <ProductCardSimple>
+                                <img src={productImg} alt="Expresso Tradicional" />
+
+                                <ProductInfo>
+                                    <ProductTitle>
+                                        <h3>Expresso Tradicional</h3>
+                                        <span>R$ 9,90</span>
+                                    </ProductTitle>
+
+                                    <ProductActions>
+                                        <InputNumber />
+                                        <ButtonSecondary>
+                                            <Trash size={16} />
+                                            Remover
+                                        </ButtonSecondary>
+                                    </ProductActions>
+                                </ProductInfo>
+                            </ProductCardSimple>
+
+                            <ProductCardSimple>
+                                <img src={productImg} alt="Expresso Tradicional" />
+
+                                <ProductInfo>
+                                    <ProductTitle>
+                                        <h3>Expresso Tradicional</h3>
+                                        <span>R$ 9,90</span>
+                                    </ProductTitle>
+
+                                    <ProductActions>
+                                        <InputNumber />
+                                        <ButtonSecondary>
+                                            <Trash size={16} />
+                                            Remover
+                                        </ButtonSecondary>
+                                    </ProductActions>
+                                </ProductInfo>
+                            </ProductCardSimple>
+
+                            <Subtotal>
+                                <span>Total de itens</span>
+                                <span>R$ 29,70</span>
+
+                                <span>Entrega</span>
+                                <span>R$ 3,50</span>
+
+                                <span>Total</span>
+                                <span>R$ 33,20</span>
+
+                                <ButtonPrimary fill>Confirmar Pedido</ButtonPrimary>
+                            </Subtotal>
+                        </CartSubtotal>
+                    </CartDetails>
+                </CheckoutForm>
+            </Container>
+        </main>
+    )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,362 @@
+import styled, { css } from 'styled-components'
+
+export const Container = styled.section`
+    max-width: calc(70rem + 4.5rem);
+    padding: 2.5rem 2.25rem;
+    margin-right: auto;
+    margin-left: auto;
+`
+
+export const CheckoutForm = styled.form`
+    width: 100%;
+    flex-direction: row;
+    display: flex;
+    gap: 2rem;
+
+    @media (max-width: 640px) {
+        flex-direction: column;
+    }
+`
+
+export const Delivery = styled.div`
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+
+    @media (max-width: 1024px) {
+        width: 50%;
+    }
+
+    @media (max-width: 640px) {
+        width: 100%;
+    }
+
+    h2 {
+        color: ${(props) => props.theme['gray-800']};
+        font-size: 1.125rem;
+        font-weight: 700;
+        line-height: 1.3;
+        margin-bottom: 1.375rem;
+    }
+`
+
+const WrapperBase = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 2rem;
+    background-color: ${(props) => props.theme['gray-100']};
+    padding: 2.5rem;
+    border-radius: 6px;
+`
+
+export const Address = styled(WrapperBase)`
+    margin-bottom: 0.75rem;
+
+    header {
+        display: flex;
+        column-gap: 0.625rem;
+
+        svg {
+            fill: ${(props) => props.theme['yellow-800']};
+        }
+
+        h3 {
+            color: ${(props) => props.theme['gray-800']};
+            font-family: ${(props) => props.theme['font-secondary']};
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.3;
+        }
+
+        p {
+            font-size: 0.875rem;
+            line-height: 1.3;
+        }
+    }
+`
+
+export const AddressFormFields = styled.div`
+    display: grid;
+    grid-template-columns: repeat(12, minmax(0, 1fr));
+    gap: 1rem 0.75rem;
+
+    > div {
+        @media (max-width: 1024px) {
+            grid-column: span 12;
+        }
+    }
+`
+
+export const InputGroup = styled.div<{ $cols?: number }>`
+    display: flex;
+    flex-direction: column;
+    ${(props) => props.$cols && `grid-column: span ${props.$cols};`}
+    position: relative;
+`
+
+export const InputLabel = styled.label<{ $srOnly?: boolean }>`
+    ${(props) => {
+        if (props.$srOnly) {
+            return css`
+                width: 1px;
+                height: 1px;
+                position: absolute;
+                padding: 0;
+                margin: -1px;
+                border-width: 0;
+                clip: rect(0, 0, 0, 0);
+                white-space: nowrap;
+                overflow: hidden;
+            `
+        } else {
+            return css`
+                font-size: 0.875rem;
+            `
+        }
+    }}
+`
+
+export const InputControl = styled.input`
+    background-color: ${(props) => props.theme['gray-200']};
+    font-size: 0.875rem;
+    padding: 0.75rem;
+    border: 1px solid ${(props) => props.theme['gray-300']};
+    border-radius: 4px;
+    transition: border 300ms ease-in-out;
+
+    & + span {
+        position: absolute;
+        top: 0;
+        right: 0.75rem;
+        color: ${(props) => props.theme['gray-500']};
+        font-size: 0.75rem;
+        font-style: italic;
+        transform: translateY(100%);
+    }
+
+    &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+        font-size: 0.875rem;
+        font-weight: 400;
+    }
+
+    &:hover,
+    &:focus {
+        border-color: ${(props) => props.theme['yellow-800']};
+
+        &::placeholder {
+            color: ${(props) => props.theme['gray-700']};
+        }
+
+        & + span {
+            width: 1px;
+            height: 1px;
+            position: absolute;
+            padding: 0;
+            margin: -1px;
+            border-width: 0;
+            clip: rect(0, 0, 0, 0);
+            white-space: nowrap;
+            overflow: hidden;
+        }
+    }
+
+    &:-webkit-autofill,
+    &:-webkit-autofill:hover,
+    &:-webkit-autofill:focus {
+        -webkit-text-fill-color: ${(props) => props.theme['gray-700']};
+        -webkit-box-shadow: 0 0 0 40rem ${(props) => props.theme['gray-200']} inset;
+    }
+`
+
+export const CheckGroup = styled(InputGroup)``
+
+export const CheckLabel = styled.label`
+    display: flex;
+    align-items: center;
+    column-gap: 0.75rem;
+    background-color: ${(props) => props.theme['gray-300']};
+    color: ${(props) => props.theme['gray-700']};
+    font-size: 0.75rem;
+    font-weight: 400;
+    line-height: 1.6;
+    text-transform: uppercase;
+    padding: 1rem;
+    border: 1px solid transparent;
+    border-radius: 6px;
+    cursor: pointer;
+    transition: background, border 300ms ease-in-out;
+
+    &:hover {
+        background-color: ${(props) => props.theme['gray-400']};
+    }
+
+    & > svg {
+        fill: ${(props) => props.theme['purple-700']};
+        margin-top: -2px;
+    }
+`
+
+export const CheckControl = styled.input`
+    position: absolute;
+    clip: rect(0, 0, 0, 0);
+    pointer-events: none;
+
+    &:checked + label {
+        background-color: ${(props) => props.theme['purple-300']};
+        border-color: ${(props) => props.theme['purple-700']};
+    }
+`
+
+export const PaymentMethods = styled(WrapperBase)`
+    header {
+        display: flex;
+        column-gap: 0.625rem;
+
+        svg {
+            fill: ${(props) => props.theme['purple-700']};
+        }
+
+        h3 {
+            color: ${(props) => props.theme['gray-800']};
+            font-family: ${(props) => props.theme['font-secondary']};
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.3;
+        }
+
+        p {
+            font-size: 0.875rem;
+            line-height: 1.3;
+        }
+    }
+
+    & > div {
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        gap: 0.75rem;
+
+        @media (max-width: 1024px) {
+            grid-template-columns: repeat(1, minmax(0, 1fr));
+        }
+
+        > div {
+            display: grid;
+        }
+    }
+`
+
+export const CartDetails = styled.div`
+    width: 40%;
+    display: flex;
+    flex-direction: column;
+
+    @media (max-width: 1024px) {
+        width: 50%;
+    }
+
+    @media (max-width: 640px) {
+        width: 100%;
+    }
+
+    h2 {
+        color: ${(props) => props.theme['gray-800']};
+        font-size: 1.125rem;
+        font-weight: 700;
+        line-height: 1.3;
+        margin-bottom: 1.375rem;
+    }
+`
+
+export const CartSubtotal = styled(WrapperBase)`
+    border-radius: 6px 36px;
+    row-gap: 0;
+`
+
+export const ProductCardSimple = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 1.25rem;
+    padding: 0.5rem 0.25rem 2.375rem;
+    margin-bottom: 1.875rem;
+    border-bottom: 1px solid ${(props) => props.theme['gray-300']};
+
+    @media (max-width: 1024px) {
+        flex-direction: column;
+    }
+
+    img {
+        width: 4rem;
+        height: 4rem;
+    }
+`
+
+export const ProductInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.5rem;
+    flex: 1;
+`
+
+export const ProductTitle = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    @media (max-width: 1280px) {
+        flex-direction: column;
+        align-items: start;
+    }
+
+    h3 {
+        color: ${(props) => props.theme['gray-800']};
+        font-family: ${(props) => props.theme['font-secondary']};
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.3;
+    }
+
+    span {
+        color: ${(props) => props.theme['gray-700']};
+        font-weight: 700;
+        line-height: 1.3;
+    }
+`
+
+export const ProductActions = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    width: 70%;
+    height: 2rem;
+`
+
+export const Subtotal = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    row-gap: 0.75rem;
+    font-weight: 400;
+
+    span:nth-of-type(even) {
+        text-align: right;
+    }
+
+    span:nth-of-type(1),
+    span:nth-of-type(3) {
+        font-size: 0.875rem;
+    }
+
+    span:nth-of-type(5),
+    span:nth-of-type(6) {
+        color: ${(props) => props.theme['gray-800']};
+        font-size: 1.25rem;
+        font-weight: 700;
+    }
+
+    button {
+        margin-top: 0.75rem;
+        grid-column: span 2;
+    }
+`
