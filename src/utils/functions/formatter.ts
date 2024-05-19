c51544fc393f8ca2b75acb965b1cf8fcfa87e1cd
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-05-19 20:39:21 -0300

    feat(formatter): create function to format zipcode

--- src/utils/functions/formatter.ts
+++ src/utils/functions/formatter.ts
@@ -7,6 +7,16 @@ const formatPrice = (amount: number) => {
   return priceFormatted.replace('R$', '').trim()
 }
 
+const formatZipcode = (value: string) => {
+  if (!value) return ''
+
+  const onlyDigits = value.replace(/\D/g, '')
+  const zipcode = onlyDigits.replace(/(\d{5})(\d)/, '$1-$2').substring(0, 9)
+
+  return zipcode
+}
+
 export const format = {
   price: formatPrice,
+  zipcode: formatZipcode,
 }
